a4c8f1c6de0ef675b47bef1a41cfc1a822354017
abdu951 <abdumoh051@gmail.com>
2025-12-22 21:09:09 +0300

    improve navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,9 @@ import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const [user, setUser, setShowUserLogin, navigate] = useAppContext()
-    const logout = () => {
+    const { user, setUser, setShowUserLogin, navigate } = useAppContext()
+
+    const logout = async () => {
         setUser(null)
         navigate('/')
     }
@@ -33,10 +34,20 @@ const Navbar = () => {
                     <img src={assets.nav_cart_icon} alt="cart" className="w-6 opacity-80" />
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
-
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                {!user ? (
+                    <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                     Login
                 </button>
+                ) : (
+                    <div className='relative group'>
+                        <img src={assets.profile_icon} alt="profile" className='w-10'/>
+                        <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md z-40 text-sm '>
+                            <li onClick={() => navigate('my orders')} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                            <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                        </ul>
+                    </div>
+                )}
+                
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -47,7 +58,7 @@ const Navbar = () => {
             {open && (
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to="/" onClick={()=> setOpen(false)}>Home</NavLink>
-                <NavLink to="/products" onClick={()=> setOpen(false)}>All products</NavLink>
+                <NavLink to="/products" onClick={()=> setOpen(false)}>All product</NavLink>
                 {user && 
                 <NavLink to="/products" onClick={()=> setOpen(false)}>My Orders</NavLink>
                 }
@@ -58,7 +69,7 @@ const Navbar = () => {
                     Login
                 </button>
                 ) : (
-                <button onClick={logout}className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
                     Logout
                 </button>
             )}
