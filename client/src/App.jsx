156cecef429c3c433347af88e2a350a067598ab4
abdu951 <abdumoh051@gmail.com>
2025-12-23 17:26:55 +0300

    add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 const App = () => {
+  const isSellerpath = useLocation().pathname.includes('seller')
   return (
     <div>
-      <Navbar />
-      <div>
+      {isSellerpath ? null : <Navbar />}
+      
+      <div className= {`${isSellerpath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
