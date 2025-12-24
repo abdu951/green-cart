3725e2c67e7ccb9a7c031bf08181a779f4bf2d72
abdu951 <abdumoh051@gmail.com>
2025-12-24 20:43:25 +0300

    add image

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const BottomBanner = () => {
   return (
     <div className='relative mt-24'>
-      
+      <img src={assets.bottom_banner_image} alt="banner" />
     </div>
   )
 }
