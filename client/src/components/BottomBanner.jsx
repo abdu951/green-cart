7e3ff2dd09ed6d2ea9bafa1898760e4b03a25d06
abdu951 <abdumoh051@gmail.com>
2025-12-25 14:07:30 +0300

    add another image

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -4,7 +4,8 @@ import { assets } from '../assets/assets'
 const BottomBanner = () => {
   return (
     <div className='relative mt-24'>
-      <img src={assets.bottom_banner_image} alt="banner" />
+      <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block'/>
+      <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full md:hidden'/>
     </div>
   )
 }
