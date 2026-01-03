acd87ce2cde1fff8233575993467e98495e5292b
abdu951 <abdumoh051@gmail.com>
2026-01-03 09:40:24 +0300

    add product controller

---
+++ server/controllers/productController.js
@@ -0,0 +1,24 @@
+
+
+// add product: api/product/add
+export const addProduct = (req, res) => {
+
+}
+
+
+// get product: api/product/list
+export const productList = (req, res) => {
+
+}
+
+
+// get single product: api/product/id
+export const ProductById = (req, res) => {
+
+}
+
+
+// add single product: api/product/add
+export const changeStock = (req, res) => {
+
+}
